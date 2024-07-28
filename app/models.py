5852280fe205c9a1f9420ffaf34a11a5b8b277dd
dilshod-81 <murodkobilovdilshod@mail.ru>
2024-07-28 22:15:33 +0500

    ok

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Index(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.first_name
+
